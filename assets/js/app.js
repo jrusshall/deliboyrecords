d9da0f9dd2058fd1227f487886db08b54da2d180
Jacob Hall <jrusshall@gmail.com>
2016-10-27 17:15:55 -0600

    new cover and bandcamp links

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('deliApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
-
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: '../views/homeTmpl.html'
-        })
-    $urlRouterProvider.otherwise('/');
-})
